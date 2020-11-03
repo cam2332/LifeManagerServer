8f9683d724f3b48e8ab978de2e1a5f75e59daa98
cam2332 <kam2332@gmail.com>
2020-11-04 00:30:36 +0100

    additional setup

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export = {
+    port: process.env.PORT || 3000,
+    databaseUrl: process.env.MONGODB_URI || '',
+    JwtSecret: process.env.JWT_SECRET || 'secret',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello world!')
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+
+const app = express();
+app.use(bodyParser.json())
+
+mongoose.connect(config.databaseUrl, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (error) => {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info('MongoDB connected')
+    }
+})
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.error('Mongoose default connection closed through app termination')
+        process.exit(0)
+    })
+})
+
+app.listen(config.port, () => {
+    console.log('Server is running at port', config.port)
+})
