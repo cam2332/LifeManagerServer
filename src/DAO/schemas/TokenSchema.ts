df9a3407db02bbf076e010040ba4e8b7ff00c0b9
cam2332 <kam2332@gmail.com>
2020-11-10 16:30:42 +0100

    Create TokenSchema.ts

---
+++ src/DAO/schemas/TokenSchema.ts
@@ -0,0 +1,40 @@
+import { Schema, Types } from 'mongoose';
+import { TokenDocument } from '../documents/TokenDocument';
+import { Token } from '../models/TokenModel';
+
+export const TokenSchema: Schema = new Schema(
+  {
+    userId: { type: Types.ObjectId, ref: 'users', required: true },
+    deviceName: { type: String, required: true },
+    createDate: { type: Date, required: true },
+    type: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { versionKey: false, collection: 'tokens' },
+);
+
+TokenSchema.statics.createToken = async (
+  token: TokenDocument,
+): Promise<TokenDocument> => {
+  return await new Token(token).save();
+};
+
+TokenSchema.statics.getByValue = async (
+  tokenValue: string,
+): Promise<TokenDocument | null> => {
+  const result = await Token.findOne({ value: tokenValue });
+  return result;
+};
+
+TokenSchema.statics.removeTokens = async (
+  userId: string,
+  deviceName: string,
+): Promise<{
+  ok?: number | undefined;
+  n?: number | undefined;
+  deletedCount?: number | undefined;
+}> => {
+  return await Token.deleteMany({
+    $and: [{ userId: Types.ObjectId(userId) }, { deviceId: deviceName }],
+  });
+};
