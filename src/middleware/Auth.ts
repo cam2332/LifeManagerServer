40e076825cfcbd75683047553435fe6327f6cf57
cam2332 <kam2332@gmail.com>
2020-11-12 11:40:59 +0100

    Update Auth.ts with putting userId in Request

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -2,36 +2,43 @@ import { Request, Response, NextFunction } from 'express';
 import ApplicationError from '../service/ApplicationError';
 import jwt from 'jsonwebtoken';
 import config from '../config';
+import { UserDocument } from '../DAO/documents/UserDocument';
 
 export const auth = (
   request: Request,
   response: Response,
   next: NextFunction,
-) => {
-  let token =
-    request.header('x-access-token') ||
-    request.header('Authorization') ||
-    request.header('authorization') ||
-    request.header('token');
-
-  if (!token) {
-    throw new ApplicationError(
-      'Access denied. No token provided.',
-      ApplicationError.UNAUTHORIZED.code,
-    );
-  }
-
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length);
-  }
-
+): void => {
   try {
-    jwt.verify(token, config.JwtSecret);
-    next();
+    let token =
+      request.header('x-access-token') ||
+      request.header('Authorization') ||
+      request.header('authorization') ||
+      request.header('token');
+
+    if (!token) {
+      throw new ApplicationError(
+        'Access denied. No token provided.',
+        ApplicationError.UNAUTHORIZED.code,
+      );
+    }
+
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length);
+    }
+
+    try {
+      const payload = jwt.verify(token, config.JwtSecret);
+      /** Put userId from token payload in request for controllers */
+      request.userId = (payload as UserDocument).id;
+      next();
+    } catch (error) {
+      throw new ApplicationError(
+        'Invalid token.',
+        ApplicationError.BAD_REQUEST.code,
+      );
+    }
   } catch (error) {
-    throw new ApplicationError(
-      'Invalid token.',
-      ApplicationError.BAD_REQUEST.code,
-    );
+    ApplicationError.errorHandler(error, response);
   }
 };
