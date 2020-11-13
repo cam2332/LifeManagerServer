fd1b9368782604b9adee1bded6ad054f1a3b88ca
cam2332 <kam2332@gmail.com>
2020-11-13 23:51:15 +0100

    Update MongoConverter.ts

--- src/service/MongoConverter.ts
+++ src/service/MongoConverter.ts
@@ -1,4 +1,6 @@
+import { Types } from 'mongoose';
 import { UserDocument } from '../DAO/documents/UserDocument';
+import { NoteDocument } from '../DAO/documents/NoteDocument';
 
 export const fromUser = (
   user: UserDocument,
@@ -16,4 +18,50 @@ export const fromUser = (
     displayName: user.displayName,
     profileImageUrl: user.profileImageUrl,
   };
+};
+
+export const toNote = (
+  title: string,
+  text: string,
+  userId: string,
+  createDate: Date = new Date(),
+  lastEditDate: Date = new Date(),
+): NoteDocument => {
+  return {
+    title: title,
+    text: text,
+    createDate: createDate,
+    lastEditDate: lastEditDate,
+    userId: Types.ObjectId(userId),
+  } as NoteDocument;
+};
+
+export const fromNote = (
+  note: NoteDocument,
+): {
+  id: string;
+  title: string;
+  text: string;
+  createDate: Date;
+  lastEditDate: Date;
+} => {
+  return {
+    id: note.id,
+    title: note.title,
+    text: note.text,
+    createDate: note.createDate,
+    lastEditDate: note.lastEditDate,
+  };
+};
+
+export const fromNoteArray = (
+  noteArray: NoteDocument[],
+): {
+  id: string;
+  title: string;
+  text: string;
+  createDate: Date;
+  lastEditDate: Date;
+}[] => {
+  return noteArray.map((note) => fromNote(note));
 };
