6e92648126759f0ec71005eb8703df79f2d6c690
cam2332 <kam2332@gmail.com>
2020-11-13 23:52:37 +0100

    Update Routes.ts with "note" route

--- src/REST/Routes.ts
+++ src/REST/Routes.ts
@@ -1,11 +1,18 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
 import UserController from './UserController';
+import NoteController from './NoteController';
 
 const routes: Router = express.Router();
 routes.use(
   '/user',
   async (request: Request, response: Response, next: NextFunction) => {
     UserController(request, response, next);
+  },
+);
+routes.use(
+  '/note',
+  async (request: Request, response: Response, next: NextFunction) => {
+    NoteController(request, response, next);
   },
 );
 
