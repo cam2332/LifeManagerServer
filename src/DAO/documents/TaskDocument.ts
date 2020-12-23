209f034c00d6d7dd97a2a2d1b8aaceb691121fc7
cam2332 <kam2332@gmail.com>
2020-12-23 12:33:02 +0100

    Create TaskDocument.ts

---
+++ src/DAO/documents/TaskDocument.ts
@@ -0,0 +1,13 @@
+import { Document, Types } from 'mongoose';
+
+export interface TaskDocument extends Document {
+  title: string;
+  categoryId?: Types.ObjectId | undefined;
+  favorite: boolean;
+  done: boolean;
+  note: string;
+  startDate?: Date | undefined;
+  endDate?: Date | undefined;
+  lastEditDate?: Date | undefined;
+  userId: Types.ObjectId;
+}
