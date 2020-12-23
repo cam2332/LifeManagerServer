3b21b1bab1bbd141393917be102dfe6097bfbbe2
cam2332 <kam2332@gmail.com>
2020-12-23 19:37:20 +0100

    Create CategorySchema.ts

---
+++ src/DAO/schemas/CategorySchema.ts
@@ -0,0 +1,26 @@
+import { Schema, Types } from 'mongoose';
+import { CategoryDocument } from '../documents/CategoryDocument';
+import { Category } from '../models/CategoryModel';
+
+export const CategorySchema: Schema = new Schema(
+  {
+    text: { type: String, required: true },
+    color: String,
+    icon: String,
+    userId: { type: Types.ObjectId, ref: 'users', required: true },
+  },
+  { versionKey: false, collection: 'categories' },
+);
+
+CategorySchema.statics.createCategory = async (
+  newCategory: CategoryDocument,
+): Promise<CategoryDocument> => {
+  const category = new Category(newCategory);
+  return category.save();
+};
+CategorySchema.statics.getByUserId = async (
+  userId: string,
+): Promise<CategoryDocument[]> => {
+  const categories = await Category.find({ userId: Types.ObjectId(userId) });
+  return categories;
+};
